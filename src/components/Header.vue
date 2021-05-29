1f2308f198917be3c1ee2dcc1aa9da822ba4389a
estacet <tkachikmarine@gmail.com>
2021-05-29 15:11:24 +0300

    Changed components structure

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,18 +1,29 @@
 <template>
-  <div>
-    <HeaderMenu/>
+  <div class="header">
+    <div class="header-menu">
+      <b-tabs class="header-menu_tab">
+        <b-tab title="City guide" class="" active>
+          <CityGuide/>
+        </b-tab>
+        <b-tab title="Favorite" class="">
+          <Favorite/>
+        </b-tab>
+      </b-tabs>
+    </div>
     <HeaderFilters/>
   </div>
 </template>
 
 <script>
-import HeaderMenu from './Header-menu'
+import CityGuide from './CityGuide'
+import Favorite from "@/components/Favorite";
 import HeaderFilters from './Header-filters'
 
 export default {
-  name: "Header",
+  name: "Header-menu",
   components: {
-    HeaderMenu,
+    CityGuide,
+    Favorite,
     HeaderFilters
   },
   data() {
@@ -21,3 +32,57 @@ export default {
   methods: {}
 }
 </script>
+
+<style lang="less" >
+.header-menu {
+  position: absolute;
+  width: 754px;
+  height: 50px;
+  left: 147px;
+  top: 141px;
+
+  background: #EEEEEE;
+
+  .header-menu_tab {
+    position: absolute;
+    width: 228px;
+    height: 38px;
+    left: 263px;
+
+    .nav-tabs {
+      margin: 12px 0;
+      border-bottom: none;
+
+      .nav-item {
+        height: 26px;
+
+        a {
+          padding: 0;
+          font-family: Helvetica;
+          font-size: 16px;
+          font-style: normal;
+          font-weight: 400;
+          line-height: 26px;
+          letter-spacing: 0em;
+          text-align: left;
+
+          background-color: unset;
+          border: none;
+
+          color:  #999999;
+        }
+
+      .active {
+         color: #333333;;
+      }
+
+        &:first-child {
+          margin-right: 98px;
+        }
+
+      }
+    }
+  }
+}
+
+</style>
